2937dd05d6e80874a5a1fb422efbb29499d76f50
Carlos Ribeiro <carlos.ribeiro.537h@gmail.com>
2017-05-31 14:30:20 -0300

    Add files via upload

---
+++ Access-CSharpConnectionExamble/Access-CSharpConnectionExamble/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Access_CSharpConnectionExamble {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dgvDataBase = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOpenFilePath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataBase)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDataBase
+            // 
+            this.dgvDataBase.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDataBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataBase.Location = new System.Drawing.Point(12, 220);
+            this.dgvDataBase.Name = "dgvDataBase";
+            this.dgvDataBase.Size = new System.Drawing.Size(740, 227);
+            this.dgvDataBase.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose your MS Access Database:";
+            // 
+            // txtOpenFilePath
+            // 
+            this.txtOpenFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOpenFilePath.Location = new System.Drawing.Point(193, 21);
+            this.txtOpenFilePath.Name = "txtOpenFilePath";
+            this.txtOpenFilePath.Size = new System.Drawing.Size(478, 20);
+            this.txtOpenFilePath.TabIndex = 2;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(677, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "MS Access DataBases|*.accdb";
+            this.openFileDialog1.Title = "Choose your MS Access Database";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConnectionString.Location = new System.Drawing.Point(10, 19);
+            this.txtConnectionString.Multiline = true;
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.ReadOnly = true;
+            this.txtConnectionString.Size = new System.Drawing.Size(714, 59);
+            this.txtConnectionString.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtConnectionString);
+            this.groupBox1.Location = new System.Drawing.Point(15, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(737, 91);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your connection string:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(609, 154);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(143, 41);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Table Name:";
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(86, 165);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(214, 20);
+            this.txtTableName.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 469);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtOpenFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDataBase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataBase)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDataBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOpenFilePath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTableName;
+    }
+}
+
---
+++ Access-CSharpConnectionExamble/Access-CSharpConnectionExamble/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Access_CSharpConnectionExamble {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        string AccessConnectionString = "";
+        string tableName = "";
+
+        private void btnOpen_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+            txtOpenFilePath.Text = openFileDialog1.FileName;
+            AccessConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;"+
+                                       "Data Source='"+openFileDialog1.FileName+"';Persist Security Info=True";
+            txtConnectionString.Text = AccessConnectionString;
+        }
+
+        public void ConnectToAccessDB() {
+            OleDbConnection AccessConnection = new OleDbConnection();
+            AccessConnection.ConnectionString = AccessConnectionString;
+            try {
+                AccessConnection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.CommandText = "SELECT * FROM "+tableName+";";
+                command.CommandType = CommandType.Text;
+                command.Connection = AccessConnection;
+
+                OleDbDataReader dataDB;
+                dataDB = command.ExecuteReader();
+
+
+                DataTable dbTable = new DataTable();
+                dbTable.Load(dataDB);
+                dgvDataBase.DataSource = dbTable;
+                dgvDataBase.Refresh();
+
+                AccessConnection.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Connection Error!\n\n" + ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e) {
+            if (AccessConnectionString == "") {
+                MessageBox.Show("Choose an Access DataBase!");
+            }
+            else if (txtTableName.Text == "") {
+                MessageBox.Show("Fill the Table Name field!");
+            }
+            else {
+                tableName = txtTableName.Text;
+                ConnectToAccessDB();
+            }
+        }
+
+
+    }
+
+   
+
+}
